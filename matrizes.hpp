054ec6df3a0c9903445f463dfe47c4582c7923ac
Jhony Luiz de Almeida <jhonyl.almeida@gmail.com>
2026-08-22 20:15:30 -0300

    Pasta de códigos desenvolvidos nas aulas (exceto resolução de exercícios)

--- matrizes.hpp
+++ matrizes.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-void multiplicarMatriz (int matriz [3][3], int multiplicador);
+void multiplicarMatriz (int matriz [3][3], int multiplicador=2);
 
 void exibirMatriz(int matriz[3][3]);
